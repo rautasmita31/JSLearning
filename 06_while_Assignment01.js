db21c3e706f50427aa0de95d587e13ad0e29bdc9
rautasmita31 <rautasmita31@gmail.com>
2023-08-01 00:11:54 +0530

    While Loop Assignment
    While Loop Assignment

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,57 @@
+console.log(`------------------------Assignment no 2------------------------------`);
+console.log(`1. WAP to print numbers from 5 to 15 by incrementing 1`);
+var num=4;
+while (num<15) {
+    num++;
+    console.log(num);
+}
+console.log(`===================================================`);
+
+console.log(`2. WAP to print numbers from 50 to 40 by decrementing 1`);
+var num=51;
+while (num>40) {
+    num--;
+    console.log(num);  
+}
+
+  console.log(`===================================================`);
+
+  console.log(`3. WAP to find first 15 odd numbers`);
+  var num=1;
+  while (num<29) {
+    num=num+2;
+    console.log(num);  
+  }
+  console.log(`===================================================`);
+
+  console.log(`4. WAP to find first 15 even numbers`);
+  var num=0;
+  while (num<30) {
+    num=num+2;
+    console.log(num);
+  }
+  console.log(`===================================================`);
+
+  console.log(`5. WAP to print table of 5`);
+  var num=0;
+  while (num<50) {
+    num=num+5;
+    console.log(num);  
+  }
+ 
+ console.log(`===================================================`);
+
+  console.log(`6. WAP to print table of 10`);
+var num=0;
+while (num<100) {
+    num=num+10;
+    console.log(num);  
+}
+  console.log(`===================================================`);
+
+  console.log(`7. WAP to print table of 10 in reverse order`);
+  var num=110;
+  while (num>10) {
+    num=num-10;
+    console.log(num);
+  }
